a332ad4918f627cb4ff703d5ace4a6e022b8db0b
iamtd <mocmoc2417@gmail.com>
2019-07-31 23:34:06 +0700

    add rain sound option

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 // NAVBAR
 function openNav() {
   document.getElementById("myNav").style.width = "100%";
+  document.getElementById('text').style.display = 'none';
+  document.getElementById('copyright').innerHTML = '<a href="https://www.facebook.com/dinhtu.dt">iamtd, 2019</a>';
 }
 
 function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
+    // document.querySelector('.settings-icons').style.opacity = '0';
+    document.getElementById("myNav").style.width = "0%";
+    document.getElementById('text').style.display = 'block';
+    document.getElementById('copyright').innerHTML = '';   
 }
 
 
@@ -15,26 +20,195 @@ function init() {
   var audio, playlist;
   
   playlist = [
-      {
-          name: 'Ngày Hạ',
-          url: 'songs/NgayHa-DiLixKBee.mp3',
-          note: 'Cho những tháng năm thương nhớ người...'
-      },
-      {
-        name: 'Phiêu Bồng',
-        url: 'songs/Tofu-PhieuBongftVovanduc.mp3',
+    {
+      name: 'When the love falls',
+      url: 'music/WhenTheLoveFalls-Yiruma.mp3',
+      artist: 'Yiruma',
+      bg: 'images/tinhtrecon.jpg',
+      note: 'Cho những tháng năm thương nhớ người...'
+    },
+    {
+        name: 'Snow Flower',
+        url: 'music/SnowFlower-KimYoon.mp3',
+        artist: 'Kim Yoon',
+        bg: 'images/emhonnhien.jpg',
         note: 'Cho những tháng năm thương nhớ người...'
     },
     {
-        name: 'Bảy Bước Tới Mùa Hè',
-        url: 'songs/BayBuocToiMuaHe-DaoLiuLoo.mp3',
+        name: 'Love',
+        url: 'music/Love-WangWei.mp3',
+        artist: 'Wang Wei',
+        bg: 'images/binhyenlaxanh.jpg',
         note: 'Cho những tháng năm thương nhớ người...'
-    },  
-    // {
-    //     name: 'Hạ Chờ',
-    //     url: 'songs/HaCho.mp3',
-    //     note: 'Cho những tháng năm thương nhớ người...'
-    // },  
+    },
+    {
+        name: 'Remember',
+        url: 'music/Remember-KimYoon.mp3',
+        artist: 'Kim Yoon',
+        bg: 'images/cunvaem.jpg',
+        note: 'Cho những tháng năm thương nhớ người...'
+    },
+    {
+        name: 'Bài hát viết cho em',
+        url: 'music/BaiHatVietChoEm-PhamHoaiNam.mp3',
+        artist: 'Kim Yoon',
+        bg: 'images/hua.jpg',
+        note: 'Cho những tháng năm thương nhớ người...'
+    },
+    {
+        name: 'Chỉ còn những mùa nhớ',
+        url: 'music/ChiConNhungMuaNho-BaoTram.mp3',
+        artist: 'Kim Yoon',
+        bg: 'images/lavang.jpg',
+        note: 'Cho những tháng năm thương nhớ người...'
+    },
+    {
+        name: 'Chuyện của ta',
+        url: 'music/ChuyenCuaTa-GleeGiaThieu.mp3',
+        artist: 'Kim Yoon',
+        bg: 'images/4.jpg',
+        note: 'Cho những tháng năm thương nhớ người...'
+    },
+    {
+        name: 'Còn tuổi nào cho em',
+        url: 'music/ConTuoiNaoChoEm-MiuLe.mp3',
+        artist: 'Kim Yoon',
+        bg: 'images/cunvaem.jpg',
+        note: 'Cho những tháng năm thương nhớ người...'
+    },
+    {
+        name: 'Đánh rơi bên hồ',
+        url: 'music/DanhRoiBenHo-ThuyChi.mp3',
+        artist: 'Kim Yoon',
+        bg: 'images/2.jpg',
+        note: 'Cho những tháng năm thương nhớ người...'
+    },
+    {
+        name: 'Nhớ mang em về',
+        url: 'music/NhoMangEmVe-Hy.mp3',
+        artist: 'Kim Yoon',
+        bg: 'images/cunvaem.jpg',
+        note: 'Cho những tháng năm thương nhớ người...'
+    },
+    {
+        name: 'Sài gòn vẫn thế',
+        url: 'music/SaiGonVanThe-PhamHoaiNam.mp3',
+        artist: 'Kim Yoon',
+        bg: 'images/gocsachnangvang.jpg',
+        note: 'Cho những tháng năm thương nhớ người...'
+    },
+    {
+        name: 'Dream Catcher',
+        url: 'music/DreamCatcher-XiaoPing.mp3',
+        artist: 'Kim Yoon',
+        bg: 'images/5.jpg',
+        note: 'Cho những tháng năm thương nhớ người...'
+    },
+    {
+        name: 'Em vẫn như ngày xưa',
+        url: 'music/EmVanNhuNgayXua-ThaiTrinh.mp3',
+        artist: 'Kim Yoon',
+        bg: 'images/xam.jpg',
+        note: 'Cho những tháng năm thương nhớ người...'
+    },
+    {
+        name: 'Giấc mơ nhẹ nhàng',
+        url: 'music/GiacMoNheNhang-ThuyChiFtMinhVuong.mp3',
+        artist: 'Kim Yoon',
+        bg: 'images/cunvaem.jpg',
+        note: 'Cho những tháng năm thương nhớ người...'
+    },
+    {
+        name: 'Dona, Dona',
+        url: 'music/JoanBaez-DonnaDonna.mp3',
+        artist: 'Kim Yoon',
+        bg: 'images/ngungoan.jpg',
+        note: 'Cho những tháng năm thương nhớ người...'
+    },
+    {
+        name: 'Lớn rồi còn khóc nhè',
+        url: 'music/LonRoiConKhocNhe-TrucNhan.mp3',
+        artist: 'Kim Yoon',
+        bg: 'images/cunvaem.jpg',
+        note: 'Cho những tháng năm thương nhớ người...'
+    },
+    {
+        name: 'Mong manh tình về',
+        url: 'music/MongManhTinhVe-ThuyChi.mp3',
+        artist: 'Kim Yoon',
+        bg: 'images/cunvaem.jpg',
+        note: 'Cho những tháng năm thương nhớ người...'
+    },
+    {
+        name: 'Morning Please Don\'t Come',
+        url: 'music/MorningPleaseDontCome-WangWei.mp3',
+        artist: 'Kim Yoon',
+        bg: 'images/1.jpg',
+        note: 'Cho những tháng năm thương nhớ người...'
+    },
+    {
+        name: 'Remember',
+        url: 'music/Remember-KimYoon.mp3',
+        artist: 'Kim Yoon',
+        bg: 'images/cunvaem.jpg',
+        note: 'Cho những tháng năm thương nhớ người...'
+    },
+    {
+        name: 'Mùa hoa bỏ lại',
+        url: 'music/MuaHoaBoLai-PhamHoaiNam.mp3',
+        artist: 'Kim Yoon',
+        bg: 'images/4.jpg',
+        note: 'Cho những tháng năm thương nhớ người...'
+    },
+    {
+        name: 'Muộn màng là từ lúc',
+        url: 'music/MuonMangLaTuLuc-NguyenHa.mp3',
+        artist: 'Kim Yoon',
+        bg: 'images/1.jpg',
+        note: 'Cho những tháng năm thương nhớ người...'
+    },
+    {
+        name: 'Ta có hẹn với tháng 5',
+        url: 'music/TaCoHenVoiThang5-Nguyen Ha.mp3',
+        artist: 'Kim Yoon',
+        bg: 'images/cunvaem.jpg',
+        note: 'Cho những tháng năm thương nhớ người...'
+    },
+    {
+        name: 'Tháng mười một',
+        url: 'music/ThangMuoiMot-Hy.mp3',
+        artist: 'Kim Yoon',
+        bg: 'images/binhyenlaxanh.jpg',
+        note: 'Cho những tháng năm thương nhớ người...'
+    },
+    {
+        name: 'Tinh cầu cô đơn',
+        url: 'music/TinhCauCoDon-PhamHoaiNam.mp3',
+        artist: 'Kim Yoon',
+        bg: 'images/1.jpg',
+        note: 'Cho những tháng năm thương nhớ người...'
+    },
+    {
+        name: 'Tình đầu',
+        url: 'music/TinhDau-NguyenHa.mp3',
+        artist: 'Kim Yoon',
+        bg: 'images/tinhtrecon.jpg',
+        note: 'Cho những tháng năm thương nhớ người...'
+    },
+    {
+        name: 'Vừa biết dấu yêu',
+        url: 'music/VuaBietDauYeu-ThanhTrangNguyen.mp3',
+        artist: 'Kim Yoon',
+        bg: 'images/binhyenlaxanh.jpg',
+        note: 'Cho những tháng năm thương nhớ người...'
+    },
+    {
+        name: 'Want to be a Pisces',
+        url: 'music/WantToBeAPisces-JinShi.mp3',
+        artist: 'Kim Yoon',
+        bg: 'images/honnhien.jpg',
+        note: 'Cho những tháng năm thương nhớ người...'
+    },
     {
         name: 'Lạc Nhau Có Phải Muôn Đời',
         url: 'songs/LacNhauCoPhaiMuonDoi-TrietPham.mp3',
@@ -152,7 +326,36 @@ function init() {
       }, false);
   }
   shake();
+
+    const btnRain = document.getElementById('btn-rain');
+    btnRain.addEventListener('click', rainPlay);
+
+    function rainPlay() {
+        var rain = document.getElementById('1');
+        console.log(rain.paused);
+        if (!rain.paused) {
+            rain.pause();
+        } else {
+            rain.play();
+        }
+    }
+
+
+    document.getElementById('btn-rain').addEventListener('click', () => {
+        document.getElementById('btn-rain').classList.toggle('settings-icons--off');
+        rainPlay;
+    });
+    document.getElementById('btn-music').addEventListener('click', () => {       
+        document.getElementById('btn-music').classList.toggle('settings-icons--off');
+        if (!audio.paused) {
+            audio.pause();
+        } else {
+            audio.play();
+        }
+    });
+    document.getElementById('btn-next').addEventListener('click', switchTrack);
 }
+
 
 
 init();
